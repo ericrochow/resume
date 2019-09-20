220433f260ccc9fb83cd2ea3447ec3926f32d169
Eric Rochow <erochow@liquidweb.com>
2019-09-20 10:09:12 -0400

    add resume configuration

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,15 +2,15 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'Eric Rochow'
-SITENAME = 'Eric Rochow Resume'
-SITEURL = ''
+AUTHOR = "Eric Rochow"
+SITENAME = "Eric Rochow Resume"
+SITEURL = ""
 
-PATH = 'content'
+PATH = "content"
 
-TIMEZONE = 'America/Detroit'
+TIMEZONE = "America/Detroit"
 
-DEFAULT_LANG = 'en'
+DEFAULT_LANG = "en"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -20,16 +20,56 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (("Website", "https://ericroc.how"),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (
+    ("You can add links in your config file", "#"),
+    ("Another social link", "#"),
+)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# RELATIVE_URLS = True
+
+# Profile information
+NAME = AUTHOR
+TAGLINE = ""
+PIC = ""
+
+# Sidebar links
+EMAIL = "ericrochow@gmail.com"
+PHONE = ""
+WEBSITE = "ericroc.how"
+LINKEDIN = "erochow"
+GITHUB = "ericrochow"
+TWITTER = "@eric_rochow"
+
+CAREER_SUMMARY = ""
+
+SKILLS = [{"title": "", "level": "90"}]
+
+PROJECT_INTRO = ""
+
+PROJECTS = [{"title": "", "tagline": ""}]
+
+INTERESTS = [""]
+
+EXPERIENCES = [
+    {
+        "job_title": "Network Engineer",
+        "time": "Oct 2011 - July 2015",
+        "company": "Spartan-Net, East Lansing, MI",
+        "details": "description goes here",
+    }
+]
+
+EDUCATIONS = [
+    {
+        "degree": "B.S. in Computer Networking",
+        "meta": "Davenport University",
+        "time": "2011",
+    },
+    {"degree": "High School", "meta": "Lansing Christian", "time": "2006"},
+]
